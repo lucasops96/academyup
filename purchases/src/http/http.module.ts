5f24eae58b26eb417894e1a897a8d00462925971
João Lucas de Sousa Martins <lucasbanana96@gmail.com>
2023-08-27 15:29:58 -0300

    configurando auth0

---
+++ purchases/src/http/http.module.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+    imports: [ConfigModule.forRoot()],
+})
+export class HttpModule {}
