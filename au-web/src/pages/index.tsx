24bce7b5918c20ec4918c865d986aa1d89348ffe
João Lucas de Sousa Martins <lucasbanana96@gmail.com>
2023-08-28 23:29:51 -0300

    auth0 nextjs login

--- au-web/src/pages/index.tsx
+++ au-web/src/pages/index.tsx
@@ -1,12 +1,15 @@
-
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
+import { useUser } from '@auth0/nextjs-auth0/client'
 export default function Home() {
+
+  const { user } = useUser()
+
   return (
     <div>
-      <h1>helllooo</h1>
+      <h1>helllooo world</h1>
+
+      <pre>{JSON.stringify(user, null,2)}</pre>
+
+      <a href="api/auth/login">Login</a>
     </div>
   )
 }
