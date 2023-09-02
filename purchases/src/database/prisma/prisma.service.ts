7493b4cbe31fc6f8f14315ba4ddd1830c3d8756f
João Lucas de Sousa Martins <lucasbanana96@gmail.com>
2023-09-02 20:04:13 -0300

    configurando docker e prisma ORM

---
+++ purchases/src/database/prisma/prisma.service.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+import {  Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    
+    // constructor() {
+    //     super()
+    // }
+    
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    // async onModuleDestroy() {
+    //     await this.$disconnect()
+    // }
+
+    // async enableShutdownHooks(app: INestApplication) {
+    //     this.$on('beforeExit', async () => {
+    //       await app.close();
+    //     });
+    // }
+    
+}
